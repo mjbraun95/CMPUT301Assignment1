18f8fbee7a31e1f898535f1ad935047040116c7b
Matthew Braun <mjbraun@ualberta.ca>
2022-02-05 21:04:33 -0700

    Fixed UI

--- RollCount/app/src/main/java/com/example/rollcount/GameSessionFragment.java
+++
@@ -1,4 +0,0 @@
-package com.example.rollcount;
-
-public class GameSessionFragment {
-}
---
+++ RollCount2/app/src/main/java/com/example/rollcount2/SecondFragment.java
@@ -0,0 +1,56 @@
+package com.example.rollcount2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.rollcount2.databinding.FragmentSecondBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+public class SecondFragment extends Fragment {
+
+    private FragmentSecondBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
